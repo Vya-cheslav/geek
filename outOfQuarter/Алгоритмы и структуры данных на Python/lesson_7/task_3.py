d1357a1cfb8a012f5ae29009079765b23043f24b
Vya-cheslav <god44@mail.ru>
2020-01-03 18:00:45 +0300

    Update task_3.py

--- outOfQuarter/Алгоритмы и структуры данных на Python/lesson_7/task_3.py
+++ outOfQuarter/Алгоритмы и структуры данных на Python/lesson_7/task_3.py
@@ -9,17 +9,21 @@ size = 2 * m + 1  # Всегда нечетный массив
 arr = [random.randint(0, 10) for _ in range(size)]
 
 print(f'Исходный массив {arr}')
-mi = len(arr) // 2 + 1
-mi_arr = []
 
-while len(arr) > 0 and mi > 0:
-    k = arr[0]
-    for j in range(len(arr)):
-       if k > arr[j]:
-            k = arr[j]
-    if k <= arr[0]:
-        mi_arr.append(k)
-        mi -= 1
-        arr.remove(k)
+def median(arr):
+    mi = len(arr) // 2 + 1
+    mi_arr = []
 
-print(f'Медиана {mi_arr[len(mi_arr) - 1]}')
+    while len(arr) > 0 and mi > 0:
+        k = arr[0]
+        for j in range(len(arr)):
+           if k > arr[j]:
+                k = arr[j]
+        if k <= arr[0]:
+            mi_arr.append(k)
+            mi -= 1
+            arr.remove(k)
+
+    print(f'Медиана {mi_arr[len(mi_arr) - 1]}')
+
+median(arr)
